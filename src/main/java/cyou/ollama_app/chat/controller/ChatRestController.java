8c9bd04241dc514171da623787a2e07cb2e9e84c
lalithk90 <asakahatapitiya@gmail.com>
2024-11-27 13:49:09 +0530

    frontend is completed

--- src/main/java/cyou/ollama_app/chat/controller/ChatRestController.java
+++ src/main/java/cyou/ollama_app/chat/controller/ChatRestController.java
@@ -3,6 +3,9 @@ package cyou.ollama_app.chat.controller;
 import cyou.ollama_app.chat.service.LlamaAiService;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.QuestionAnswerAdvisor;
+import org.springframework.ai.chat.client.advisor.SafeGuardAdvisor;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.chat.client.advisor.VectorStoreChatMemoryAdvisor;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,17 +13,28 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.Collections;
+import java.util.List;
+
 @RestController
 public class ChatRestController {
 
   private final ChatClient chatClient;
   private final LlamaAiService aiService;
 
+  List<String> sensitiveWords = Collections.singletonList("fuck");
+
   public ChatRestController(ChatClient.Builder builder, VectorStore vectorStore, LlamaAiService aiService) {
     this.aiService = aiService;
     this.chatClient = builder
-        .defaultAdvisors(new QuestionAnswerAdvisor(vectorStore))
+        .defaultAdvisors(
+            new QuestionAnswerAdvisor(vectorStore),
+            new SafeGuardAdvisor(sensitiveWords),
+            new SimpleLoggerAdvisor(),
+            new VectorStoreChatMemoryAdvisor(vectorStore)
+        )
         .build();
+
   }
 
   @GetMapping("/chat")
